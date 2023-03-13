d8692917cd758443920fa604e10b90e4b14f8e16
空余梦中人 <106437727+KianaMyWife@users.noreply.github.com>
2023-03-13 13:16:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ while True:
     # - top-p : 默认为1 一般不动,控制聊天机器人在生成响应时选择的标记的数量。较高的top_p值会导致响应的多样性和创造性增加，但也可能会导致响应不准确或不相关。
     # - n : 为每个输入消息生成多少个聊天完成选项,默认为1
     # - stream : 是否以流式数据发送,默认为false
-    # - stop : string 或者 array,m默认为null具体没看明白什么意思,原为:Up to 4 sequences where the API will stop generating further tokens.
+    # - stop : 类型为string 或者 array,默认为null,具体没看明白什么意思,原文为:Up to 4 sequences where the API will stop generating further tokens.
     # - max_token : 返回的最大token长度,最长为4096,默认没说多少,较高的max_tokens值会产生更长的响应，但也可能会导致响应不相关或不准确。
     # - presence_penalty : 控制聊天机器人生成响应时是否使用特定的词汇或主题。较高的presence_penalty值会导致聊天机器人更少地使用特定的词汇或主题。
     # - frequency_penalty : 控制聊天机器人生成响应时在文本中重复使用相同词汇的程度。较高的frequency_penalty值会导致聊天机器人更少地重复使用相同的词汇。
